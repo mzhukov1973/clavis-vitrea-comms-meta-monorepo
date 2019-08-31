ba5b5380742b7ea3a6565110ea48470b1cf78742
mzhukov1973 <mzhukov31415dev@gmail.com>
2019-08-31 23:44:15 +0300

    Another meta commit.

--- meta-commit-push.sh
+++ meta-commit-push.sh
@@ -2,6 +2,6 @@
 
 COMMIT_MESSAGE="Another meta commit."
 
-meta git add --all .
-meta git commit -a -m "$COMMIT_MESSAGE"
-meta git push
+meta-git add --all .
+meta-git commit -a -m "$COMMIT_MESSAGE"
+meta-git push
